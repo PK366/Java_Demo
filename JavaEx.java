8dbae7ce02242f0d76aecc241716367a0e259f5f
pramod <pramodk935@gmail.com>
2024-06-29 16:49:00 +0530

    done

---
+++ JavaEx.java
@@ -0,0 +1,15 @@
+public class JavaEx {
+	public static void main(String[] args) {
+		String firstName = "Pramod";
+		String lastName = "Kumar";
+		String fullName = firstName + lastName;
+		int x = 15;
+		int y = 25;
+		int z = x + y;
+		int a = z * 5;
+		System.out.println(fullName);
+		System.out.println(a);
+		System.out.println(z);
+	}
+}
+
